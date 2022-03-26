30182d18979cdcd08ecf76489e53d9f758a6d299
Alan A. F. Lopes <alan.alb.flopes@gmail.com>
2022-03-26 10:08:47 -0400

    refactor: cleaned jwtGenerator

--- Utils/JwtGenerator.js
+++ Utils/JwtGenerator.js
@@ -1,10 +1,8 @@
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-const jwtConfig = {
-  expiresIn: '1d',
-};
-
-const SECRET = process.env.JWT_SECRET;
-
-module.exports = (displayName, email) => jwt.sign({ displayName, email }, SECRET, jwtConfig);
+module.exports = (displayName, email) => jwt.sign(
+    { displayName, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '1d' },
+);
